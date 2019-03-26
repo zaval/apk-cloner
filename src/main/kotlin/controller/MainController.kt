e26745cbdcdbcabcdcf6fe872ebba147eaec07b1
zaval <dzavalny@gmail.com>
2019-03-26 03:06:18 +0200

    apksig from repo

--- src/main/kotlin/controller/MainController.kt
+++ src/main/kotlin/controller/MainController.kt
@@ -7,11 +7,12 @@ import brut.androlib.err.InFileNotFoundException
 import brut.androlib.err.OutDirExistsException
 import brut.common.BrutException
 import brut.directory.DirectoryException
+import javafx.beans.Observable
+import javafx.collections.FXCollections
+import javafx.collections.ObservableList
 import utils.TextAreaHandler
 import javafx.fxml.FXML
-import javafx.scene.control.Button
-import javafx.scene.control.TextArea
-import javafx.scene.control.TextField
+import javafx.scene.control.*
 import javafx.scene.layout.VBox
 import javafx.stage.FileChooser
 import java.io.File
@@ -38,19 +39,73 @@ class MainController {
     @FXML
     lateinit var loggerText: TextArea
 
+    @FXML
+    lateinit var devicesCombo: ComboBox<String>
+
+    @FXML
+    lateinit var updateDevices: Button
+
+    @FXML
+    lateinit var appsList: ListView<String>
+
+    @FXML
+    lateinit var mainTab: TabPane
+
 
     private var logger = Logger.getLogger(Androlib::class.java.name)
 
-    fun switchUI(enable: Boolean){
+    private fun switchUI(enable: Boolean){
         browseApk.isDisable = !enable
         apkFname.isDisable = !enable
         cloneApk.isDisable = !enable
+        updateDevices.isDisable = !enable
+        devicesCombo.isDisable = !enable
+        appsList.isDisable = !enable
     }
 
 
+    lateinit var  lines: ObservableList<String>
+
+    private fun updateAdbDevices(){
+        val res = GlobalScope.async {
+            lines = FXCollections.observableList(utils.Hlp.getAdbDevices())
+
+        }
+
+        runBlocking {
+            res.await()
+            devicesCombo.items = lines
+            if (lines.size > 0)
+                devicesCombo.selectionModel.select(0)
+
+        }
+
+        loadApps()
+
+
+    }
+
+    lateinit var apps: ObservableList<String>
+
+    private fun loadApps(){
+
+        if (devicesCombo.selectionModel.isEmpty)
+            return
+        val res = GlobalScope.async {
+            apps = FXCollections.observableList(utils.Hlp.getAdbInstalledApps(devicesCombo.selectionModel.selectedItem))
+        }
+
+        runBlocking {
+            res.await()
+            appsList.items = apps
+            println(apps)
+        }
+
+    }
+
     fun initialize(){
 
-        utils.Hlp.processApk("asd")
+        updateAdbDevices()
 
         logger.addHandler(TextAreaHandler(loggerText))
 
@@ -65,24 +120,73 @@ class MainController {
 
         cloneApk.setOnAction {
 
-            cloneApk.isDisable = true
-            loggerText.appendText("*** Start cloning file ${apkFname.text} ***\n")
+            var apkPathText = apkFname.text
+            loggerText.appendText("*** Start cloning file ${apkPathText} ***\n")
+
+
+            GlobalScope.launch{
 
 
 
-             GlobalScope.launch{
+
+                 if (mainTab.selectionModel.selectedIndex == 1){
+                     if (devicesCombo.selectionModel.isEmpty){
+//                         switchUI(true)
+                         logger.info("*** Clone finished ***\n")
+                         return@launch
+                     }
+
+                     val tmpPath = "/tmp/" + Random.nextInt(100, 999).toString()
+                     File(tmpPath).mkdirs()
+                     apkPathText = utils.Hlp.downloadAdbApp(devicesCombo.selectionModel.selectedItem, appsList.selectionModel.selectedItem ?: "", tmpPath)
+                     if (apkPathText.isEmpty())
+                     {
+//                         switchUI(true)
+                         logger.info("*** Clone finished ***\n")
+                         return@launch
+                     }
+
+                 }
 
                  switchUI(false)
                  try {
-                     utils.Hlp.processApk(apkFname.text)
+                     utils.Hlp.processApk(apkPathText)
                  } catch (e: Exception){
                      loggerText.appendText("@@@ Can't clone this apk @@@\n${e.localizedMessage}")
+                 }
+
+                 if (mainTab.selectionModel.selectedIndex == 1){
+
+                     val fnameToInstall = File(apkPathText).parentFile.listFiles {
+                         _, name ->
+
+                         name.endsWith("-signed.apk")
+
+                     }
+
+                     println(fnameToInstall.size)
+
+                     if (fnameToInstall.isNotEmpty()){
+                         logger.info("*** Uploading application ***\nCheck, maybe you need some action on your device!")
+                         utils.Hlp.installAdbApp(devicesCombo.selectionModel.selectedItem, fnameToInstall[0]?.absolutePath ?: "")
+                     }
+
                  }
 
                  switchUI(true)
                  loggerText.appendText("*** Clone finished ***\n")
 
              }
+
+        }
+
+        updateDevices.setOnAction {
+            updateAdbDevices()
+        }
+
+        devicesCombo.selectionModel.selectedItemProperty().addListener {
+                _: Observable ->
+                loadApps()
 
         }
 
--- src/main/kotlin/utils/Hlp.kt
+++ src/main/kotlin/utils/Hlp.kt
@@ -10,32 +10,53 @@ import brut.common.BrutException
 import brut.directory.DirectoryException
 import com.android.apksig.ApkSigner
 import com.android.apksig.apk.MinSdkVersionException
-import com.android.apksigner.PasswordRetriever
-import com.android.apksigner.SignerParams
 import java.io.File
 import java.io.IOException
+import java.util.concurrent.TimeUnit
 import java.util.logging.Logger
 import kotlin.random.Random
+import java.security.KeyFactory
+import java.security.PrivateKey
+import java.security.spec.PKCS8EncodedKeySpec
+import java.util.*
+import sun.security.provider.X509Factory
+import java.io.ByteArrayInputStream
+import java.security.cert.CertificateFactory
+import java.security.cert.X509Certificate
+
+
+private val adbPath = System.getenv("ADB") ?: ((System.getenv("HOME") ?: "/home/user") + "/Android/Sdk/platform-tools/adb")
+
+fun String.asResource(): String {
+    return javaClass::class.java.getResource(this).readText()
+}
 
 fun sign(apkName: String) :Boolean {
 
     val newFname = apkName.replace(Regex("\\.apk$"), "-signed.apk")
+    val signerConfigs = ArrayList<ApkSigner.SignerConfig>(1)
 
-    val signers = java.util.ArrayList<SignerParams>(1)
-    val signerParams = SignerParams()
-    signerParams.setKeystoreFile("/home/zaval/projects/android-reverse/key.jks")
-    signerParams.setKeystorePasswordSpec("pass:123123")
-    signers.add(signerParams)
+    var privateKeyContent = "/private_key.pem".asResource()
+    privateKeyContent = privateKeyContent.replace("\n", "").replace("-----BEGIN PRIVATE KEY-----", "").replace("-----END PRIVATE KEY-----", "")
+    val kf = KeyFactory.getInstance("RSA")
+    val keySpecPKCS8 = PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKeyContent))
+    val privKey: PrivateKey = kf.generatePrivate(keySpecPKCS8)
 
-    val signerConfigs = ArrayList<ApkSigner.SignerConfig>(signers.size)
+    var publicKeyContent = "/cert.pem".asResource()
+    publicKeyContent = publicKeyContent.replace("\n", "").replace("-----BEGIN PUBLIC KEY-----", "")
+        .replace("-----END PUBLIC KEY-----", "")
 
-//    val keyFileName = File(signers[0].keyFile).name
 
-    var passwordRetriever = PasswordRetriever()
-    signers[0].loadPrivateKeyAndCerts(passwordRetriever)
+    val decoded =
+        Base64.getDecoder().decode(publicKeyContent.replace(X509Factory.BEGIN_CERT, "").replace(X509Factory.END_CERT, ""))
+
+    val cert: X509Certificate = CertificateFactory.getInstance("X.509")?.generateCertificate(ByteArrayInputStream(decoded)) as X509Certificate
+
+    val certificates = ArrayList<X509Certificate>(1)
+    certificates.add(cert)
 
     val signerConfig = ApkSigner.SignerConfig.Builder(
-        "key", signers[0].privateKey, signers[0].certs
+        "key", privKey, certificates.toList()
     )
         .build()
     signerConfigs.add(signerConfig)
@@ -108,7 +129,6 @@ fun processApk(apkName: String): Boolean{
 
     val manifestFile = File(outDir.absolutePath + "/AndroidManifest.xml")
     if (!manifestFile.exists()){
-//        cloneApk.isDisable = false
         return false
     }
 
@@ -118,7 +138,6 @@ fun processApk(apkName: String): Boolean{
     val packageName = regex.find(manifestData)?.groups?.get(1)?.value ?: ""
 
     if (packageName.isEmpty()){
-//        cloneApk.isDisable = false
         return false
     }
 
@@ -144,7 +163,7 @@ fun processApk(apkName: String): Boolean{
         val fPath = tmpF.absolutePath ?: ""
 
         val newFPath = fPath.replace(smaliPackageName, smaliPackageNameNew)
-        if (!fPath.equals(newFPath)){
+        if (fPath != newFPath){
             tmpF.copyTo(File(newFPath))
             tmpF.delete()
         }
@@ -171,8 +190,79 @@ fun processApk(apkName: String): Boolean{
 
     sign(resultFileName)
 
-    outDir.deleteRecursively()
+//    outDir.deleteRecursively()
 
     return true
 
+}
+
+
+fun String.execute(workingDir: File): String {
+    try {
+        val parts = this.split("\\s".toRegex())
+        val proc = ProcessBuilder(*parts.toTypedArray())
+            .directory(workingDir)
+            .redirectOutput(ProcessBuilder.Redirect.PIPE)
+            .redirectError(ProcessBuilder.Redirect.PIPE)
+            .start()
+
+        proc.waitFor(2, TimeUnit.MINUTES)
+        return proc.inputStream.bufferedReader().readText()
+    } catch(e: Exception) {
+        e.printStackTrace()
+        return ""
+    }
+}
+
+fun parseAll(pattern: Regex, str: String): List<String>{
+    val result = ArrayList<String>()
+
+    var match = pattern.find(str)
+    while (match != null){
+        result.add(match.groupValues[1])
+        match = match.next()
+    }
+    return result.toList()
+}
+
+fun parse(pattern: Regex, str: String): String{
+
+    val match = pattern.find(str)
+    return match?.groupValues?.get(1) ?: ""
+
+}
+
+
+
+fun getAdbDevices(): List<String>{
+
+    val adbResult = "$adbPath devices".execute(File("."))
+    println(adbResult)
+
+    return parseAll("(\\S+)\tdevice".toRegex(), adbResult)
+
+}
+
+fun getAdbInstalledApps(device: String): List<String>{
+    val adbResult = "$adbPath -s $device shell pm list packages".execute(File("."))
+    val result = parseAll("package:(\\S+)".toRegex(), adbResult)
+    return  result
+}
+
+fun downloadAdbApp(device: String, app: String, path: String): String{
+    if (app.isEmpty())
+        return ""
+    println(app)
+    val adbResult = "$adbPath -s $device shell pm path $app".execute(File("."))
+
+    val packagePath =  parse("package:(\\S+)".toRegex(), adbResult)
+
+    val res = "$adbPath -s $device pull $packagePath $path".execute(File("."))
+    println(res)
+    return "$path/${File(packagePath).name}"
+}
+
+fun installAdbApp(device: String, apk: String){
+    val res = "$adbPath -s $device install $apk".execute(File("."))
+    println(res)
 }
