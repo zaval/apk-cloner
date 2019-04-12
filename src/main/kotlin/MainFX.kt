0f37eecb2c6ebb55c09d50f870d133e689a7567c
zaval <dzavalny@gmail.com>
2019-04-13 02:22:59 +0300

    dark theme

--- src/main/kotlin/MainFX.kt
+++ src/main/kotlin/MainFX.kt
@@ -11,12 +11,15 @@ class Main : Application() {
 
     private val layout = "/main.fxml"
     private val icon = "/logo.png"
+    private val cssFile = "/modena.css";
 
     override fun start(primaryStage: Stage?) {
         System.setProperty("prism.lcdtext", "false")
         primaryStage?.title = "APK Cloner"
         primaryStage?.scene = Scene(load<Parent?>(Main.javaClass.getResource(layout)))
-
+        val css = Main.javaClass.getResource(cssFile)
+//        println(css)
+        primaryStage?.scene?.stylesheets?.add(css.toString())
         primaryStage?.icons?.add(Image(Main.javaClass.getResourceAsStream(icon)))
         primaryStage?.show()
     }
